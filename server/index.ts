5ec1af18ae47705f7721befe3838b0935039fb7b
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-07-03 02:47:08 -0300

    :Bugfix: fixing port error

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,6 @@ import App from './api';
 
 const app = new App().express;
 
-app.listen(() => {
-  console.log('App ouvindo na porta 3000');
+app.listen(3001, () => {
+  console.log('App ouvindo na porta 3001');
 });
