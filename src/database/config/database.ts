e1b292a05e6659ad594e2da8dbdaa23e8f3f6db1
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-07-03 21:42:36 -0300

    :sparkles: creating connection to db

---
+++ src/database/config/database.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+};
+
+export = config;
