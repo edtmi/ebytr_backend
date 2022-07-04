1ce8dd718859184e248d6b3e318ec4d3d0c2577b
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-07-03 22:41:00 -0300

    :sparkles: creating method get

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -6,6 +6,7 @@ const taskRoute = Router();
 const taskController = new TaskController();
 
 taskRoute.route('/')
-  .post(taskController.createTask);
+  .post(taskController.createTask)
+  .get(taskController.listAllTasks);
 
 export default taskRoute;
